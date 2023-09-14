897f3cb083bb4368f19eeac4b8595520bb658580
petershlee <nzpetershlee@gmail.com>
2023-09-14 20:16:18 +1000

    create unit test

---
+++ CodingChallenge.Server/StreamService.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingChallenge.Server
+{
+    public class StreamService
+    {
+        public static IList<int> FindLongestIncreasingSequence(string stream)
+        {
+            return Enumerable.Empty<int>().ToList();
+        }
+    }
+}
